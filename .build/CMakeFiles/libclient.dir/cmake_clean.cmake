14541517311f2cbfa711839d70f2aa02c8702fe1
Michal <michalurb8@wp.pl>
2020-05-23 18:00:37 +0200

    Test commit

---
+++ .build/CMakeFiles/libclient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libclient.dir/libraries/client/lib.cpp.o"
+  "liblibclient.a"
+  "liblibclient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
